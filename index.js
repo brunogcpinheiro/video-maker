829d1472471bcd2e2a0cbd3b50866edc1b326230
Bruno Carpinelli <brunogcpinheiro@hotmail.com>
2019-03-16 13:23:09 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
+const robots = {
+	text: require("./robots/text"),
+};
 
-function start () {
-    const content = {};
-    
-    content.searchTerm = askAndReturnSearchTerm();
-    content.prefix = askAndReturnPrefix();
-    
-    function askAndReturnSearchTerm () {
-        return readline.question('Type a Wikipedia search term: ');
-    }
-    
-    function askAndReturnPrefix () {
-        const prefixes = ['Who is', 'What is', 'The history of'];
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option:');
-        const selectedPrefixText = prefixes[selectedPrefixIndex];
-        
-        return selectedPrefixText;
-    }
-    
-    console.log(content);
+async function start () {
+	const content = {};
+
+	content.searchTerm = askAndReturnSearchTerm();
+	content.prefix = askAndReturnPrefix();
+
+	await robots.text(content);
+
+	function askAndReturnSearchTerm () {
+		return readline.question("Type a Wikipedia search term: ");
+	}
+
+	function askAndReturnPrefix () {
+		const prefixes = [ "Who is", "What is", "The history of" ];
+		const selectedPrefixIndex = readline.keyInSelect(
+			prefixes,
+			"Choose one option:",
+		);
+		const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+		return selectedPrefixText;
+	}
+
+	console.log(content);
 }
 
 start();
